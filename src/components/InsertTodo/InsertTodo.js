0f55472e6e5971befd37fc2cb018d380deecdb5c
ChaudharyHimani <chaudhary24himani@gmail.com>
2021-06-03 20:48:20 +0530

    Edit Functionality Completed

--- src/components/InsertTodo/InsertTodo.js
+++ src/components/InsertTodo/InsertTodo.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./InsertTodo.css";
 import { v1 } from 'uuid';
 
-const InsertTodo = ({todos,setTodos}) => {
+const InsertTodo = ({todos,setTodos, task, edit, onChange}) => {
     const [todo, setTodo] = useState("");
 
     const handleSubmit = (e) => {
@@ -14,13 +14,24 @@ const InsertTodo = ({todos,setTodos}) => {
         setTodos([...todos, newTodo]);
         setTodo("");
     }
-
+    useEffect(() => {
+        task && task.name && setTodo(task.name)
+    }, [task])
+    
+    console.log(edit)
     return (
         <div className="insertTodo">
+        { edit ? <form onSubmit={handleSubmit}> 
+                <input type="text" value={todo} onChange={(e) => {
+                    setTodo(e.target.value)}
+                 } placeholder="" name="add" />
+                <button type="submit" onClick= {() => onChange()} >Edit</button>
+            </form> :
             <form onSubmit={handleSubmit}> 
                 <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="" name="add" />
                 <button type="submit">Add</button>
             </form>
+        }
         </div>
     );
 }
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+import InsertTodo from '../InsertTodo/InsertTodo';
 import './Todo.css';
 
 const Todo = ({todo, setTodos, todos}) => {
@@ -6,13 +8,21 @@ const Todo = ({todo, setTodos, todos}) => {
             return item.id !== todo.id;
         }));
     }
+    const[edit, setEdit] = useState(false)
+
+    const editTodo = () => {
+        setEdit(true)
+    }
     return (
+        edit ? <InsertTodo edit task={todo} todos ={todos} setTodos = {setTodos} onChange = {() => {
+            setEdit(false)
+        } }/> : 
         <div className="Todo">
             <div className="todo__name">
                 <h4>{todo.name}</h4>
             </div>
             <div className="todo__buttons">
-                <button >Edit</button>
+                <button onClick={editTodo}>Edit</button>
                 <button onClick={deleteTodo}>Delete</button>
             </div>
         </div>
